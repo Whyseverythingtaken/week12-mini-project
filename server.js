3b90d1232b8ee18a7ca09f4089a187461c9caaad
Hamed Ali <123hamed@gmail.com>
2022-06-25 13:34:32 +0100

    add working code for /api/movies endpoint

--- server.js
+++ server.js
@@ -5,12 +5,30 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "the_best_database",
+  },
+  console.log("Connected to Databse the_best_database")
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // It's done when the /api/movies route renders a list of all movies.
 app.get("/api/movies", (request, response) => {
-  response.send("Hello");
+  console.log("Incomming GET request to /api/movies");
+
+  db.query("SELECT * FROM movies;", (err, result) => {
+    if (err) {
+      response.status(500).send("Something went wrong");
+    } else {
+      response.status(200).json(result);
+    }
+  });
 });
 
 app.listen(port, () => {
