5bb94b5503ac127742b5e640a57ebf2265e577d9
Hamed Ali <123hamed@gmail.com>
2022-06-25 14:11:46 +0100

    add code for /api/update-review request

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.post("/api/add-movie", (request, response) => {
   console.log("Incomming POST request to /api/add-movie", request.body);
   const { movie_name } = request.body;
 
+  if (!movie_name) {
+    response.send("Please supply a valid movie_name property");
+  }
+
   // Add it to SQL database
   db.query(
     "INSERT INTO movies (movie_name) VALUES (?)",
@@ -52,6 +56,48 @@ app.post("/api/add-movie", (request, response) => {
         } else {
           response.status(500).send("Unable to create new movie record");
         }
+      }
+    }
+  );
+});
+
+app.get("/api/reviews", (request, response) => {
+  console.log("Incomming GET request to /api/reviews");
+
+  db.query("SELECT * FROM reviews;", (err, result) => {
+    if (err) {
+      response.status(500).send("Something went wrong");
+    } else {
+      response.status(200).json(result);
+    }
+  });
+});
+
+// It's done when the /api/update-review route successfully updates a movie when tested using Insomnia.
+app.post("/api/update-review", (request, response) => {
+  console.log("Incomming POST request to /api/update-review", request.body);
+  const { id, review } = request.body;
+  if (!id || !review) {
+    response.send(
+      'Please supply a valid review object with an "id" and "review" properties'
+    );
+  }
+
+  db.query(
+    "INSERT INTO reviews (movie_id, review) VALUES (?, ?)",
+    [id, review],
+    (error, results) => {
+      if (error) {
+        response
+          .status(500)
+          .send(`Error adding review for movie with id = ${id}`);
+        console.log(error);
+      } else {
+        console.log(results);
+        response.status(201).json({
+          status: "success",
+          body: request.body,
+        });
       }
     }
   );
