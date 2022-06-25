c1f142cbc3dcb75b861b89707ec4d6a4ce9c8561
Hamed Ali <123hamed@gmail.com>
2022-06-25 13:18:40 +0100

    added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (id, movie_name)
+VALUES (1, "RUSH HOUR 1"),
+(2, "RUSH HOUR 2"),
+(3, "RUSH HOUR 3"),
+(4, "Kabhi Khushi Kabhi Gham"),
+(5, "Mohabbatain");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, 'This is a great movie!'),
+(2, 'This is also a great movie!'),
+(3, 'This was a bit of a drag'),
+(4, 'Bollywood?! Are you kidding me'),
+(5, 'Sharukh Khan is the best?');
